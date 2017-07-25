a328484135a322fb3765c8868e0c9e56f5cd8d71
Mathias Kluba <mathias.kluba@gmail.com>
2017-07-25 22:14:06 +0200

    deploy the cluster

--- deploy.py
+++ deploy.py
@@ -36,8 +36,8 @@ ambari_client = AmbariClient()
 
 ambari_client.connect(ambari_public_ip, stack_name)
 
-#ambari_client.create_cluster('configs/ambari', cluster_size, hdp_repo_url=hdp_repo_url, hdp_util_repo_url=hdp_util_repo_url)
+ambari_client.create_cluster('configs/ambari', cluster_size, hdp_repo_url=hdp_repo_url, hdp_util_repo_url=hdp_util_repo_url)
 
 #ambari_client.dump_config('configs/ambari', cluster_size)
 
-ambari_client.update_cluster('configs/ambari', cluster_size)
+#ambari_client.update_cluster('configs/ambari', cluster_size)
